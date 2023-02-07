0349d04a84ae003a43be24cb226bd37684795ca4
mishranisha-17 <nisha17soin@gmail.com>
2023-02-07 17:59:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+//console.log("Hello")
+// alert("Hello");
+//  document.write("Hello");
+//  var a = parseInt(prompt("Enter first no."));
+//  var b = parseInt(prompt("Enter second no."));
+//   alert(a+b);
+// addition:
+// var a = Number(prompt("Enter first no."));
+// var b = Number(prompt("Enter second no."));
+// alert(a+b);
+
+// substraction:
+
+// var a = Number(prompt("Enter first no."));
+// var b = Number(prompt("Enter second no."));
+// alert(a-b);
+
+// multiply:
+
+// var a = Number(prompt("Enter first no."));
+// var b = Number(prompt("Enter second no."));
+// alert(a*b);
+
+// division:
+
+// var a = Number(prompt("Enter first no."));
+// var b = Number(prompt("Enter second no."));
+// alert(a/b);
+
+ confirm("Do you want to continue?");
+function test(){
+    var click = prompt("Enter your name");
+    alert(click);
+}
+var click = parseInt(prompt("Enter first no."));
+ var click2 = parseInt(prompt("Enter second no."));
